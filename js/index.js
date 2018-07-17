78ded4f92b1a10f6b9fed2517a7c77adbc82ff2a
李祖明 <solace.lee@outlook.com>
2018-07-17 22:35:27 +0800

    第一次更新

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+}
+	function  menu(){
+		var menustyle=document.getElementsByClassName("minimenu");
+		menustyle[0].style.display='block';
+		
+	}
+
+	function menuhiden(){
+		var menustyle=document.getElementsByClassName("minimenu");
+		menustyle[0].style.display='none';
+	}
