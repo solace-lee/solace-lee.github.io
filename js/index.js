ae7e88f06cd3a6e935e408d5fd8a59ba95520bc1
solacelee <solace.lee@outlook.com>
2018-07-18 17:22:42 +0800

    1

--- js/index.js
+++ js/index.js
@@ -10,3 +10,8 @@ window.onload=function(){
 		var menustyle=document.getElementsByClassName("minimenu");
 		menustyle[0].style.display='none';
 	}
+
+	function more(e){
+		e.style.display="none";
+		e.parent.children[2].style.overflow="hidden"
+	}
