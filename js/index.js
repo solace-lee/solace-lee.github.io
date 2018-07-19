a09b3b46229ea4dd0ea81f566556882947af8acb
solacelee <solace.lee@outlook.com>
2018-07-19 16:47:26 +0800

    text more

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
-window.onload=function(){
+//window.onload=function(){
+//}
+//	function  menu(){
+//		var menustyle=document.getElementsByClassName("minimenu");
+//		menustyle[0].style.display='block';
+//		
+//	}
+
+//	function menuhiden(){
+//		var menustyle=document.getElementsByClassName("minimenu");
+//		menustyle[0].style.display='none';
+//	}
+
+
+$(window).load(function(){	
+	$('.minimenu').click(function(){
+		$('.minimenu').css('display','none');
+	})
+})
+
+function menu(){
+	$('.minimenu').css('display','block');
 }
-	function  menu(){
-		var menustyle=document.getElementsByClassName("minimenu");
-		menustyle[0].style.display='block';
-		
-	}
 
-	function menuhiden(){
-		var menustyle=document.getElementsByClassName("minimenu");
-		menustyle[0].style.display='none';
-	}
-
-	function more(e){
-		e.style.display="none";
-		e.parent.childnodes[2].style.overflow="hidden"
-	}
+function more(e){
+	e.style.display="none";
+	$(e).parent().find('.txt').css('display','block');
+	console.log() 
+}
