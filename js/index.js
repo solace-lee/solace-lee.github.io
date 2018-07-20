1ef585bed80c3e8f163c43037bc0530c1d6850a4
solacelee <solace.lee@outlook.com>
2018-07-20 17:27:45 +0800

    Jason

--- js/index.js
+++ js/index.js
@@ -1,21 +1,28 @@
-$(window).load(function(){	
-	$('.minimenu').click(function(){
-		$('.minimenu').css('display','none');
+$(window).load(function() {
+	$.getJSON("jason/database.json", function(data) {
+		var $left = $('.left');
+		$.each(data, function(infoindex,info) {
+			$left.append('<div class="title"><a href="'+info["code_link"]+'">'+info["title"]+'</a></div>');
+		});
 	})
 })
 
-function menu(){
-	$('.minimenu').css('display','block');
+$('.minimenu').click(function() {
+	$('.minimenu').css('display', 'none');
+})
+
+function menu() {
+	$('.minimenu').css('display', 'block');
 }
 
-function more(e){
-	e.style.display="none";
-	$(e).parent().find('.txt').css('display','block');
-	$('.hide').css('display','block');
+function more(e) {
+	e.style.display = "none";
+	$(e).parent().find('.txt').css('display', 'block');
+	$('.hide').css('display', 'block');
 }
 
-function hide(e){
-	e.style.display="none";
-	$(e).parent().find('.txt').css('display','-webkit-box');
-	$('.more').css('display','block')
+function hide(e) {
+	e.style.display = "none";
+	$(e).parent().find('.txt').css('display', '-webkit-box');
+	$('.more').css('display', 'block')
 }
