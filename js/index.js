8ae1097d8c81bf698614a9048d12697ad884d649
李祖明 <solace.lee@outlook.com>
2018-07-22 17:59:17 +0800

    自动填充标签名

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 //声明一个全局对象存储Jason数据
 var database = {};
+//声明一个全局数组存取标签
+var tag=[];
 
-//网页加载完毕后动态加载database中的数据到首页
 $(window).load(function() {
+	main();
+})
+
+
+
+//加载首页主数据和最近更新的数据
+function main() {
 	$.getJSON("jason/database.json", function(data) {
 		database = data;
 		var $left = $('.left');
@@ -17,7 +25,6 @@ $(window).load(function() {
 		} else {
 			//如果Jason中的数据大于5条则取最前的5条数据append到left中
 			for(var i = 0; i < 5; i++) {
-				//console.log(database[i].title);
 				var limit = '<div class="article"><div class="title"><a href="">' + database[i].title + '</a></div><div class="info"><div class="time">' + database[i].date + '</div><div class="tag">' + database[i].label + '</div></div><div class="txt">' + database[i].content + '</div><div class="more" onclick="more(this)">MORE ></div><div class="hide" onclick="hide(this)">HIDE ></div></div>';
 				//同时将标题也追加到右侧最近更新中去
 				var limitrecent = '<li><a href="#">' + database[i].title + '</a></li>';
@@ -25,34 +32,42 @@ $(window).load(function() {
 				$('.recent-ul').append(limitrecent);
 			}
 		}
-
-		//		var line=data[0];
-		//		var article='<div class="article"><div class="title"><a href="main.html">node.js入门-1-简介</a></div><div class="info"><div class="time">2018-7-16</div><div class="tag">node.js</div></div><div class="txt">Node.js 前言 Node.js之父 Ryan Dahl : 2009年推出Node.js, 2012年退出Node.js, 转战Go语言 Node.js出现的背景</div><div class="more" onclick="more(this)">MORE ></div><div class="hide" onclick="hide(this)">HIDE ></div></div>';
-		//		$left.append(article);
-		//		$.each(data, function(infoindex,info) {
-		//		$left.append('<div class="title"><a href="'+info["code_link"]+'">'+info["title"]+'</a></div>');
-		//		});
+			tagname();
 	})
-})
+};
+
+
+//获取Jason数据流中的标签名,并把存储标签名的数组进行排序并去重，然后append到首页
+function tagname(){
+	$.each(database,function(index,tags){
+		tag.push(tags['label']);
+	});
+	$.unique(tag.sort()); 
+	for(var i=0;i<tag.length;i++){
+	var label='<li><a href="#">'+tag[i]+'</a></li>';	
+	$('.label-ul').append(label);
+	};
+};
+
 
 //响应式-小屏状态下的菜单按钮动作
 $('.minimenu').click(function() {
 	$('.minimenu').css('display', 'none');
-})
+});
 
 function menu() {
 	$('.minimenu').css('display', 'block');
-}
+};
 
 //主页中显示更多和隐藏的动作
 function more(e) {
 	e.style.display = "none";
 	$(e).parent().find('.txt').css('-webkit-line-clamp', '10');
 	$(e).parent().find('.hide').css('display', 'block');
-}
+};
 
 function hide(e) {
 	e.style.display = "none";
 	$(e).parent().find('.txt').css('-webkit-line-clamp', '3');
 	$(e).parent().find('.more').css('display', 'block')
-}
+};
